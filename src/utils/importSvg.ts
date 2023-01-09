37a0ebaf92556d8cb74da3dbfedd8549850d068e
Denis Chumachenko <deniskachumachenko1@gmail.com>
2023-01-09 07:06:06 +0200

    init

---
+++ src/utils/importSvg.ts
@@ -0,0 +1,33 @@
+import diagram from '../assets/svg/diagram.svg';
+import albums from '../assets/svg/albums.svg';
+import profile from '../assets/svg/profile.svg';
+import message from '../assets/svg/message.svg';
+import main from '../assets/svg/main.svg';
+import sound from '../assets/svg/sound.svg';
+import arrow from '../assets/svg/arrow-bottom.svg';
+import notification from '../assets/svg/notification.svg';
+import next from '../assets/svg/next.svg';
+import prev from '../assets/svg/prev.svg';
+import play from '../assets/svg/play.svg';
+import arrowBottom from '../assets/svg/arrow.svg';
+import burger from '../assets/svg/burger.svg';
+import search from '../assets/svg/search.svg';
+import headphones from '../assets/svg/headphones.svg';
+
+export {
+  diagram,
+  albums,
+  profile,
+  message,
+  main,
+  sound,
+  arrow,
+  notification,
+  next,
+  prev,
+  play,
+  arrowBottom,
+  burger,
+  search,
+  headphones
+};
---
+++ src/utils/routes.ts
@@ -0,0 +1,18 @@
+export const HOME = '/';
+export const LOGIN = '/login';
+export const ARTIST = '/artist/:name';
+export const REGISTER = '/register';
+export const TOP_COLLECTORS = '/top-collectors';
+export const DEALS = '/deals';
+export const COLLECTOR = '/collector';
+export const MARKETPLACE = '/marketplace';
+
+export const PUBLIC_ROUTE = [
+    { path: HOME, Component: '' },
+    { path: LOGIN, Component: '' },
+    { path: REGISTER, Component: '' },
+    { path: ARTIST, Component: '' },
+    { path: COLLECTOR, Component: '' },
+    { path: TOP_COLLECTORS, Component: '' },
+    { path: MARKETPLACE, Component: '' }
+];
