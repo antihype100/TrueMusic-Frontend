0eb019ae9fc060049c466d7bfa13e590aeea8bca
maksm <rayhatr3d@yandex.ru>
2023-01-10 07:39:28 +0300

    Добавили страницу исполнителя(треков), сделать роуты

--- src/pages/author/tracks/Tracks.tsx
+++ src/pages/author/tracks/Tracks.tsx
@@ -1,1 +1,35 @@
-export {};
+import { useLocation } from 'react-router-dom';
+import { TrackList } from '../../../components/trackList/TrackList';
+import { AUTHOR_ABOUT, AUTHOR_ALBUMS, AUTHOR_TRACKS } from '../../../utils/routes';
+import styles from './Tracks.module.scss';
+
+interface ITracks {
+  author: string;
+}
+
+const links = [
+  { id: 'tracks', href: AUTHOR_TRACKS, title: 'Треки' },
+  { id: 'albums', href: AUTHOR_ALBUMS, title: 'Альбомы' },
+  { id: 'about', href: AUTHOR_ABOUT, title: 'Описание' }
+];
+
+const Tracks = ({ author }: ITracks) => {
+  const { pathname } = useLocation();
+  const path = String(pathname.split('/').splice(-1));
+
+  return (
+    <section className={styles.wrapper}>
+      <h1 className={styles.title}>{author}</h1>
+      <div className={styles.links}>
+        {links.map(({ title, href, id }) => (
+          <a key={id} className={path === id ? styles.active : ''} href={href}>
+            {title}
+          </a>
+        ))}
+      </div>
+      <TrackList amountTracks={5} />
+    </section>
+  );
+};
+
+export default Tracks;
