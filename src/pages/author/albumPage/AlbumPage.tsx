4818885e0a3622138b3e9db66a536a0465365817
maksm <rayhatr3d@yandex.ru>
2023-01-12 19:09:05 +0300

    Пофиксил страницу альбома

--- src/pages/author/albumPage/AlbumPage.tsx
+++ src/pages/author/albumPage/AlbumPage.tsx
@@ -1,21 +1,33 @@
-import img from '../../../assets/album.png';
-import TrackAlbumPlaylistCover from '../../../components/trackAlbumPlaylistCover/TrackAlbumPlaylistCover';
-import { TrackList } from '../../../components/trackList/TrackList';
-import React from 'react';
+import React from "react";
+import img from "../../../assets/album.png";
+import TrackAlbumPlaylistCover from "../../../components/trackAlbumPlaylistCover/TrackAlbumPlaylistCover";
+import { TrackList } from "../../../components/trackList/TrackList";
+import BaseLayout from "../../../components/layouts/baseLayout/BaseLayout";
+import AuthorLayout from "../../../components/layouts/authorLayout/AuthorLayout";
+
+const tracksList = [
+  { author: "Kizaru", trackName: "Messege", duration: 534, likes: 234234, audition: 846532 },
+  { author: "6BLACK", trackName: "Ex Calling", duration: 234, likes: 3244, audition: 9805 },
+  { author: "Instasamka", trackName: "Pop star", duration: 349, likes: 243674, audition: 35325 },
+  { author: "Cold carti", trackName: "ILY", duration: 434, likes: 234234, audition: 324679 },
+  { author: "DJ Snake", trackName: "Middle", duration: 134, likes: 34324, audition: 65332 },
+  { author: "Kizaru", trackName: "Мне это не нужно", duration: 224, likes: 8709, audition: 2167976 }
+];
 
 const AlbumPage = () => {
   return (
-    <TrackAlbumPlaylistCover
-      img={img}
-      author={'Kizaru'}
-      likes={'241'}
-      listening={'124001'}
-      name={'Relax'}
-      release={'16 декабря 2016'}
-    >
-      <TrackList amountTracks={5} />
-    </TrackAlbumPlaylistCover>
-  );
+    <BaseLayout>
+      <AuthorLayout>
+        <TrackAlbumPlaylistCover
+        img={img}
+        likes={"241"}
+        listening={"124001"}
+        trackName={"Relax"} />
+      <TrackList amountTracks={5} tracksList={tracksList} />
+    </AuthorLayout>
+</BaseLayout>
+)
+  ;
 };
 
 export default AlbumPage;
