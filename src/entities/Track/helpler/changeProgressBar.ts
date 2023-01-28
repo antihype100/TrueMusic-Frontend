583f7a202b2399ff95067f4e11a8b9998247c76a
maksm <rayhatr3d@yandex.ru>
2023-01-28 12:38:44 +0300

    Фиксим интерфейсы, менял назваия интерфейсов в глобал сторе

--- src/entities/Track/helpler/changeProgressBar.ts
+++ src/entities/Track/helpler/changeProgressBar.ts
@@ -1,18 +1,16 @@
-import { ITrackInfoGlobal } from '../../../widgets/Player/model/globalTrackStore';
 import { RefObject } from 'react';
+import { IGlobalTrackInfo } from '../../../widgets/Player/model/globalTrackStore';
 
 export const changeProgressBar = (
     authorName: string,
-    trackInfoGlobal: ITrackInfoGlobal,
+    trackInfoGlobal: IGlobalTrackInfo,
     trackName: string,
     audioRefGlobal: RefObject<HTMLAudioElement> | null,
 
-) => {
-    return (e: any) => {
+) => (e: any) => {
         if (authorName === trackInfoGlobal.authorName && trackName === trackInfoGlobal.trackName) {
             if (audioRefGlobal !== null && audioRefGlobal.current) {
                 audioRefGlobal.current.currentTime = e.target.value
             }
         }
     }
-}
